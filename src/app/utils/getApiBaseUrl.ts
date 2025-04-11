8b43f1898691f6df853a97a6f007b33148c4faaf
mateopolci <mateopolci@hotmail.com>
2025-04-11 20:26:38 -0300

    Deploy

--- src/app/utils/getApiBaseUrl.ts
+++ src/app/utils/getApiBaseUrl.ts
@@ -1,5 +1,6 @@
 export function getApiBaseUrl(): string {
     return (
-        process.env.NEXT_PUBLIC_API_URL || "https://claudiavasquezstrapi-production.up.railway.app/"
+        //process.env.NEXT_PUBLIC_API_URL || "https://claudiavasquezstrapi-production.up.railway.app/"
+        process.env.NEXT_PUBLIC_API_URL || "https://strapi-production-5a0a.up.railway.app/"
     );
 }
